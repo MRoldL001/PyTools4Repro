ac0c5267cd034be76bbc946ac4f65909e1f3de78
Winter Lee <MRoldL001@qq.com>
2025-08-26 13:29:06 +0800

    Update and rename img2png to img2png.py

--- tools/img2png.py
+++ tools/img2png.py
@@ -5,7 +5,7 @@ from PIL import Image
 for fname in os.listdir("."):
     if fname.lower().endswith((".jpg", ".jpeg", ".webp")):
         img = Image.open(fname).convert("RGB")
-        out = os.path.splitext(fname)[0] + ".png"
-        img.save(out, "PNG")
+        out = os.path。splitext(fname)[0] + ".png"
+        img.save(out， "PNG")
         os.remove(fname)
         print(f"Converted {fname} -> {out}")
